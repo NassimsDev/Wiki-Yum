2447d24f744b238e232baaaae53c7c3284da875c
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-03-13 00:17:39 +0100

    ✨feat: add initial box page structure and styles

--- src/pages/BoxPage/BoxPage.jsx
+++ src/pages/BoxPage/BoxPage.jsx
@@ -1,8 +1,19 @@
+import "./BoxPage.css"
+import cloche from "../../assets/img/cloche.png"
+import starPink from "../../assets/img/star-pink.png"
+
 export function BoxPage() {
-    return (
-        <div>
-            <h1>Box Page</h1>
-            <p>Contenu de la page de box</p>
+  return (
+    <main>
+      <div className="box-page">
+        <div className="box-page__image-group">
+          <img src={cloche} className="box-page__cloche" alt="cloche" />
+          <img src={starPink} className="box-page__star box-page__star--large" alt="etoile rose" />
+          <img src={starPink} className="box-page__star box-page__star--big" alt="etoile rose" />
+          <img src={starPink} className="box-page__star box-page__star--medium" alt="etoile rose" />
+          <img src={starPink} className="box-page__star box-page__star--small" alt="etoile rose" />
         </div>
-    );
+      </div>
+    </main>
+  )
 }
