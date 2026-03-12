69369a6633cc5f0e107cfaa195926fbadb5cc780
Nassimovic <nassim.saa@hotmail.fr>
2026-03-12 17:05:24 +0100

    feat: adding QuizHeader and QuizAnswers components start for quiz page (temporarly in app jsx)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
+import data from "./data/boxes-data.json";
+import "./styles/index.css";
+
+const currentBox = data.box.mystery.entry;
+const question = currentBox.quiz.question;
+const allAnswers = currentBox.quiz.answers;
+const imgSrc = currentBox.imageDish;
+
+// 1 Composant QuizHeader
+function QuizHeader() {
+    return (
+        <div className="Quiz-Header">
+            <img src={imgSrc} alt="Illustration" />
+            <h1> {question} </h1>
+        </div>
+    );
+}
+
+// 2  Composant QuizAnswers
+function QuizAnswers({ children }) {
+    return (
+        <div className="Quiz-Answers">
+            <button className="Quiz-Answers-Childs">{children}</button>
+        </div>
+    );
+}
+
+// La page Quiz
 function App() {
     return (
-        <main>
-            <h1>
-                La plus grande difficulté de l'éducation, c'est
-                transformer les idées en expériences
-            </h1>
+        <main className="Quiz-Container">
+            <QuizHeader />
+
+            {allAnswers.map((ans, index) => (
+                <QuizAnswers key={index}>{ans.text}</QuizAnswers>
+            ))}
         </main>
     );
 }
