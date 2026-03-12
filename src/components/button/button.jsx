6889999d9efc4e4e145556f7087b01b4116782b0
Mageswaran Thikana <mageswaranthikana0@gmail.com>
2026-03-12 19:15:24 +0100

    style : update style CSS

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,16 +1,29 @@
- import './Button.css';
+import { useNavigate } from 'react-router-dom'; 
+import './Button.css';
 
-function Button({ texte, destination }) {
+function Button({ texte, destination, action, variante = "normal" }) {
   const navigate = useNavigate();
+
+  const gererClic = () => {
+    if (destination) {
+      // S'il y a une "destination", on prend le routeur pour changer de page
+      navigate(destination);
+    } else if (action) {
+      // Sinon, s'il y a une "action", on fait le travail demandé (ex: vérifier le quiz)
+      action();
+    }
+  };
 
   return (
     <button 
-      className="bouton-style" 
-      onClick={() => navigate(destination)}
+      // On utilise la méthode BEM 
+      className={`btn btn--${variante}`} 
+      
+      onClick={gererClic} 
     >
       {texte}
     </button>
   );
 }
 
-export default Bouton;
+export default Button;
