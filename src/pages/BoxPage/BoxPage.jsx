9e7ec32a0fc1a78de472d20ce4823d6b534773e5
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-03-13 02:12:51 +0100

    ✨feat: add BoxDishes component and navigation button

--- src/pages/BoxPage/BoxPage.jsx
+++ src/pages/BoxPage/BoxPage.jsx
@@ -1,18 +1,28 @@
 import "./BoxPage.css"
-import cloche from "../../assets/img/cloche.png"
+import bell from "../../assets/img/bell.png"
 import starPink from "../../assets/img/star-pink.png"
+import ButtonLink from "../../components/ButtonLink/ButtonLink"
+import { BoxDishes } from "../../components/BoxDishes/BoxDishes"
+import data from "../../data/boxes-data.json"
+
+console.log(`/menu/${data.box.mystery.id}`)
 
 export function BoxPage() {
   return (
     <main>
       <div className="box-page">
         <div className="box-page__image-group">
-          <img src={cloche} className="box-page__cloche" alt="cloche" />
+          <img src={bell} className="box-page__bell" alt="cloche" />
+
+          <BoxDishes></BoxDishes>
+
           <img src={starPink} className="box-page__star box-page__star--large" alt="etoile rose" />
           <img src={starPink} className="box-page__star box-page__star--big" alt="etoile rose" />
           <img src={starPink} className="box-page__star box-page__star--medium" alt="etoile rose" />
           <img src={starPink} className="box-page__star box-page__star--small" alt="etoile rose" />
         </div>
+
+        <ButtonLink texte="Ouvrir" destination={`/menu/${data.box.mystery.id}`}></ButtonLink>
       </div>
     </main>
   )
