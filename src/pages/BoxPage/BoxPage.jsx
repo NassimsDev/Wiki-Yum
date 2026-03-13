3f010c42585ed65d6ca4ba18e681839360e33924
Nassimovic <nassim.saa@hotmail.fr>
2026-03-13 15:53:31 +0100

    � fix: moving images and fonts into public for deployement issues

--- src/pages/BoxPage/BoxPage.jsx
+++ src/pages/BoxPage/BoxPage.jsx
@@ -1,60 +1,84 @@
-import "./BoxPage.css"
+import "./BoxPage.css";
 
-import { useRef } from "react"
-import gsap from "gsap"
-import { useGSAP } from "@gsap/react"
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
-import bell from "../../assets/img/bell.png"
-import starPink from "../../assets/img/star-pink.png"
-import data from "../../data/boxes-data.json"
+import bell from "/img/bell.png";
+import starPink from "/img/star-pink.png";
+import data from "../../data/boxes-data.json";
 
-import { ButtonLink } from "../../components/ButtonLink/ButtonLink"
-import { BoxDishes } from "../../components/BoxDishes/BoxDishes"
+import { ButtonLink } from "../../components/ButtonLink/ButtonLink";
+import { BoxDishes } from "../../components/BoxDishes/BoxDishes";
 
 export function BoxPage() {
-  // Étiquettes pour GSAP
+    // Étiquettes pour GSAP
 
-  const boxList = []
-  for (const key in data.box) {
-    boxList.push(key)
-  }
+    const boxList = [];
+    for (const key in data.box) {
+        boxList.push(key);
+    }
 
-  const bellRef = useRef(null)
-  const dishesRef = useRef(null)
+    const bellRef = useRef(null);
+    const dishesRef = useRef(null);
 
-  // Animations
-  useGSAP(() => {
-    // Cloche
-    gsap.from(bellRef.current, {
-      scale: 0,
-      duration: 1.0,
-      ease: "elastic.out",
-      delay: 0.5,
-    })
+    // Animations
+    useGSAP(() => {
+        // Cloche
+        gsap.from(bellRef.current, {
+            scale: 0,
+            duration: 1.0,
+            ease: "elastic.out",
+            delay: 0.5,
+        });
 
-    // Dishes
+        // Dishes
 
-    // Étoiles
-  })
+        // Étoiles
+    });
 
-  return (
-    <main>
-      <div className="box-page">
-        <div className="box-page__image-group">
-          <img ref={bellRef} src={bell} className="box-page__bell" alt="cloche" />
+    return (
+        <main>
+            <div className="box-page">
+                <div className="box-page__image-group">
+                    <img
+                        ref={bellRef}
+                        src={bell}
+                        className="box-page__bell"
+                        alt="cloche"
+                    />
 
-          <div ref={dishesRef}>
-            <BoxDishes></BoxDishes>
-          </div>
+                    <div ref={dishesRef}>
+                        <BoxDishes></BoxDishes>
+                    </div>
 
-          <img src={starPink} className="box-page__star box-page__star--large" alt="etoile rose" />
-          <img src={starPink} className="box-page__star box-page__star--big" alt="etoile rose" />
-          <img src={starPink} className="box-page__star box-page__star--medium" alt="etoile rose" />
-          <img src={starPink} className="box-page__star box-page__star--small" alt="etoile rose" />
-        </div>
+                    <img
+                        src={starPink}
+                        className="box-page__star box-page__star--large"
+                        alt="etoile rose"
+                    />
+                    <img
+                        src={starPink}
+                        className="box-page__star box-page__star--big"
+                        alt="etoile rose"
+                    />
+                    <img
+                        src={starPink}
+                        className="box-page__star box-page__star--medium"
+                        alt="etoile rose"
+                    />
+                    <img
+                        src={starPink}
+                        className="box-page__star box-page__star--small"
+                        alt="etoile rose"
+                    />
+                </div>
 
-        <ButtonLink text="Ouvrir" destination={`/menu/${boxList[0]}`}></ButtonLink>
-      </div>
-    </main>
-  )
+                <ButtonLink
+                    text="Ouvrir"
+                    destination={`/menu/${boxList[0]}`}
+                ></ButtonLink>
+            </div>
+        </main>
+    );
 }
