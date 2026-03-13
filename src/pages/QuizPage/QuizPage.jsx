2edd64113dfaa8fb9d475ad8b3f5ddcabbff1f0c
Nassimovic <nassim.saa@hotmail.fr>
2026-03-13 13:34:02 +0100

    �fix: rootes loop and starting page

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,46 +1,50 @@
-import { useParams, useNavigate } from "react-router-dom"
-import { QuizHeader } from "../../components/QuizHeader/QuizHeader.jsx"
-import { QuizAnswers } from "../../components/QuizAnswers/QuizAnswers.jsx"
-import { ButtonLink } from "../../components/ButtonLink/ButtonLink.jsx"
-import data from "../../data/boxes-data.json"
+import { useParams, useNavigate } from "react-router-dom";
+import { QuizHeader } from "../../components/QuizHeader/QuizHeader.jsx";
+import { QuizAnswers } from "../../components/QuizAnswers/QuizAnswers.jsx";
+import { ButtonLink } from "../../components/ButtonLink/ButtonLink.jsx";
+import data from "../../data/boxes-data.json";
 
-import "./QuizPage.css"
+import "./QuizPage.css";
 
 export function QuizPage() {
-  const { box, course } = useParams()
-  const navigate = useNavigate()
+    const { box, course } = useParams();
+    const navigate = useNavigate();
 
-  const meal = data.box[box][course]
-  const courses = ["entry", "main", "dessert"]
+    const meal = data.box[box][course];
+    const courses = ["entry", "main", "dessert"];
 
-  console.log("meal ->", meal)
+    console.log("meal ->", meal);
 
-  const link = course === "dessert" ? "/box" : `/quiz/${box}/${courses[meal.id + 1]}`
+    const link =
+        course === "dessert" ? "/" : `/quiz/${box}/${courses[meal.id + 1]}`;
 
-  console.log("LINK", link)
+    console.log("LINK", link);
 
-  function handleAnswerClick(answer) {
-    if (answer.correct) {
-      navigate(link)
-    } else {
-      console.log("mauvaise réponse")
+    function handleAnswerClick(answer) {
+        if (answer.correct) {
+            navigate(link);
+        } else {
+            console.log("mauvaise réponse");
+        }
     }
-  }
 
-  return (
-    <main className="Quiz-Container">
-      <QuizHeader imageDish={meal.imageDish} question={meal.quiz.question} />
+    return (
+        <main className="Quiz-Container">
+            <QuizHeader
+                imageDish={meal.imageDish}
+                question={meal.quiz.question}
+            />
 
-      <div className="Quiz-Answers-List">
-        {meal.quiz.answers.map((answer, index) => (
-          <ButtonLink
-            key={index}
-            text={answer.text}
-            action={() => handleAnswerClick(answer)}
-            variante="reponse"
-          ></ButtonLink>
-        ))}
-      </div>
-    </main>
-  )
+            <div className="Quiz-Answers-List">
+                {meal.quiz.answers.map((answer, index) => (
+                    <ButtonLink
+                        key={index}
+                        text={answer.text}
+                        action={() => handleAnswerClick(answer)}
+                        variante="reponse"
+                    ></ButtonLink>
+                ))}
+            </div>
+        </main>
+    );
 }
--- src/router.jsx
+++ src/router.jsx
@@ -1,15 +1,15 @@
 // Associe chaque URL à sa page
-import { createBrowserRouter } from "react-router-dom"
-import { BoxPage } from "./pages/BoxPage/BoxPage"
-import { MenuPage } from "./pages/MenuPage/MenuPage"
-import { ZoomPage } from "./pages/ZoomPage/ZoomPage"
-import { MealPage } from "./pages/MealPage/MealPage"
-import { QuizPage } from "./pages/QuizPage/QuizPage"
+import { createBrowserRouter } from "react-router-dom";
+import { BoxPage } from "./pages/BoxPage/BoxPage";
+import { MenuPage } from "./pages/MenuPage/MenuPage";
+import { ZoomPage } from "./pages/ZoomPage/ZoomPage";
+import { MealPage } from "./pages/MealPage/MealPage";
+import { QuizPage } from "./pages/QuizPage/QuizPage";
 
 export const router = createBrowserRouter([
-  { path: "/box", element: <BoxPage /> },
-  { path: "/menu/:box", element: <MenuPage /> },
-  { path: "/zoom/:box/:course", element: <ZoomPage /> },
-  { path: "/meal/:box/:course", element: <MealPage /> },
-  { path: "/quiz/:box/:course", element: <QuizPage /> },
-])
+    { path: "/", element: <BoxPage /> },
+    { path: "/menu/:box", element: <MenuPage /> },
+    { path: "/zoom/:box/:course", element: <ZoomPage /> },
+    { path: "/meal/:box/:course", element: <MealPage /> },
+    { path: "/quiz/:box/:course", element: <QuizPage /> },
+]);
