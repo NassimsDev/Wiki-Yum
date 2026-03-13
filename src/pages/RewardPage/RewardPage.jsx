d17b7df40fda7a9b896fe5d9bc3d621e38317752
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-03-13 18:25:34 +0100

    🐛fix: navigation after quiz

--- src/pages/RewardPage/RewardPage.jsx
+++ src/pages/RewardPage/RewardPage.jsx
@@ -5,28 +5,25 @@ import dish2 from "../../assets/img/dish-ours-soldat.png"
 import dish3 from "../../assets/img/dish-epidemie-dansante.png"
 
 export function RewardPage() {
-    return (
-        <div className="reward-container">
-            <header className="header-reward">
-                <h1 className="bravo-title">Bravo !</h1>
-                
-                <div className="dishes-row">
-                     <img src={dish1} alt="plat informatique" className="small-dish" />
-                     <img src={dish2} alt="plat joconde" className="main-dish" />
-                     <img src={dish3} alt="plat ours" className="small-dish" />
-                </div>
+  return (
+    <div className="reward-container">
+      <header className="header-reward">
+        <h1 className="bravo-title">Bravo !</h1>
+        <h1 className="bravo-title">Bravo !</h1>
 
-                <p className="reward-text">
-                    Tu as répondu correctement à toutes les questions !
-                </p>
-            </header>
-
-            <main className="reward-main">
-                <h2 className="gain-title">Tu as gagné</h2>
-                <div className="xp-badge">
-                    +10XP
-                </div>
-            </main>
+        <div className="dishes-row">
+          <img src={dish1} alt="plat informatique" className="small-dish" />
+          <img src={dish2} alt="plat joconde" className="main-dish" />
+          <img src={dish3} alt="plat ours" className="small-dish" />
         </div>
-    );
+
+        <p className="reward-text">Tu as répondu correctement à toutes les questions !</p>
+      </header>
+
+      <main className="reward-main">
+        <h2 className="gain-title">Tu as gagné</h2>
+        <div className="xp-badge">+10XP</div>
+      </main>
+    </div>
+  )
 }
