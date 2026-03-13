1aba657a2a2c50ff41041dcd51616479e752d4bc
GuillaumeZhu <guillaume.zhu@outlook.com>
2026-03-13 13:18:06 +0100

    ✨feat: animation delay progress

--- src/pages/BoxPage/BoxPage.jsx
+++ src/pages/BoxPage/BoxPage.jsx
@@ -1,6 +1,8 @@
 import "./BoxPage.css"
 
 import { useRef } from "react"
+import { useNavigate } from "react-router-dom"
+
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
 
@@ -12,15 +14,17 @@ import { ButtonLink } from "../../components/ButtonLink/ButtonLink"
 import { BoxDishes } from "../../components/BoxDishes/BoxDishes"
 
 export function BoxPage() {
-  // Étiquettes pour GSAP
-
+  // Params à envoyer
   const boxList = []
   for (const key in data.box) {
     boxList.push(key)
   }
 
+  // Étiquettes pour GSAP
   const bellRef = useRef(null)
   const dishesRef = useRef(null)
+  const containerRef = useRef(null)
+  const btnRef = useRef(null)
 
   // Animations
   useGSAP(() => {
@@ -29,18 +33,41 @@ export function BoxPage() {
       scale: 0,
       duration: 1.0,
       ease: "elastic.out",
-      delay: 0.5,
+      delay: 0,
     })
 
-    // Dishes
+    // Plats
+    gsap.from(dishesRef.current, {
+      opacity: 0,
+      duration: 1.2,
+      ease: "out",
+      delay: 2.0,
+    })
 
     // Étoiles
+    gsap.from(".box-page__star", {
+      scale: 0,
+      rotation: 180,
+      duration: 0.8,
+      ease: "back.out(1.7)",
+      stagger: 0.15,
+      delay: 0.4,
+    })
+
+    // Bouton
+    gsap.from(btnRef.current, {
+      y: 50,
+      opacity: 0,
+      duration: 0.8,
+      ease: "power2.out",
+      delay: 0.8,
+    })
   })
 
   return (
     <main>
       <div className="box-page">
-        <div className="box-page__image-group">
+        <div className="box-page__image-group" ref={containerRef}>
           <img ref={bellRef} src={bell} className="box-page__bell" alt="cloche" />
 
           <div ref={dishesRef}>
@@ -53,7 +80,9 @@ export function BoxPage() {
           <img src={starPink} className="box-page__star box-page__star--small" alt="etoile rose" />
         </div>
 
-        <ButtonLink text="Ouvrir" destination={`/menu/${boxList[0]}`}></ButtonLink>
+        <div ref={btnRef}>
+          <ButtonLink text="Ouvrir" destination={`/menu/${boxList[0]}`}></ButtonLink>
+        </div>
       </div>
     </main>
   )
